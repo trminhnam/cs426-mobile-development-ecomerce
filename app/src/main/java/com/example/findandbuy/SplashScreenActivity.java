5fa16cb8a6e50307e480809f955c6cd00d9c2bfc
thanhduycao <thanhduycao1202@gmail.com>
2022-07-01 10:04:39 +0700

    features: finish draft for splash screen

---
+++ app/src/main/java/com/example/findandbuy/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.findandbuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(() -> {
+            //This method will be executed once the timer is over
+            // Start your app main activity
+            Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(i);
+            // close this activity
+            finish();
+        }, 1500);
+    }
+}
