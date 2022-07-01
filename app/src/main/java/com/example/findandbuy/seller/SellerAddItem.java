0a6c9b7c9698a22152ed692e48cfa87f3492e581
trminhnam <trminhnam20082002@gmail.com>
2022-07-01 11:03:38 +0700

    UI: Seller Add item

---
+++ app/src/main/java/com/example/findandbuy/seller/SellerAddItem.java
@@ -0,0 +1,16 @@
+package com.example.findandbuy.seller;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.findandbuy.R;
+
+public class SellerAddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_add_item);
+    }
+}
