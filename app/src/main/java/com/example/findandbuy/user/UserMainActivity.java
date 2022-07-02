7e13eede2550f67489e6730bd6aae28d6580d5d6
thanhduycao <thanhduycao1202@gmail.com>
2022-07-02 16:54:49 +0700

    fix add fragment collapse another in container when switch tab in user main activity

--- app/src/main/java/com/example/findandbuy/user/UserMainActivity.java
+++ app/src/main/java/com/example/findandbuy/user/UserMainActivity.java
@@ -10,7 +10,10 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.findandbuy.R;
+import com.example.findandbuy.fragment.GameFragment;
+import com.example.findandbuy.fragment.ProfileFragment;
 import com.example.findandbuy.fragment.ShopFragment;
+import com.example.findandbuy.fragment.ShoppingCartFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -32,24 +35,55 @@ public class UserMainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment;
+                //Delete all fragment in container before switch tab
+                for (Fragment fragmentInContainer : getSupportFragmentManager().getFragments()) {
+                    if (fragmentInContainer != null) {
+                        getSupportFragmentManager().beginTransaction().remove(fragmentInContainer).commit();
+                    }
+                }
                 switch (item.getItemId()) {
                     case R.id.navigation_shop:
                         Log.d("shop", "onNavigationItemSelected: Im shop");
+                        if (savedInstanceState == null) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .add(R.id.frame_container, ShopFragment.class, null)
+                                    .commit();
+                        }
                         return true;
                     case R.id.navigation_game:
                         Log.d("game", "onNavigationItemSelected: Im game");
+                        if (savedInstanceState == null) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .add(R.id.frame_container, GameFragment.class, null)
+                                    .commit();
+                        }
                         return true;
                     case R.id.navigation_cart:
                         Log.d("card", "onNavigationItemSelected: Im card");
+                        if (savedInstanceState == null) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .add(R.id.frame_container, ShoppingCartFragment.class, null)
+                                    .commit();
+                        }
                         return true;
                     case R.id.navigation_profile:
                         Log.d("profile", "onNavigationItemSelected: Im profile");
+                        if (savedInstanceState == null) {
+                            getSupportFragmentManager().beginTransaction()
+                                    .setReorderingAllowed(true)
+                                    .add(R.id.frame_container, ProfileFragment.class, null)
+                                    .commit();
+                        }
                         return true;
                 }
                 return true;
             }
         });
     }
+
 
 
 }
